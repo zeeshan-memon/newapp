80c5743e3c3e45a40c27a70dd1920eb4a1092d8b
zeeshan-memon <52953436+zeeshan-memon@users.noreply.github.com>
2021-11-10 11:45:35 +0500

    pagination and spinner added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+   
     render() {
+        let {title, description, imageUrl, newUrl} = this.props;
         return (
-            <div>
-                This is a news item.
+           <div>
+                <div className="card" style={{width: "18rem"}}>
+                <img src={imageUrl?imageUrl:"https://cdn.pixabay.com/photo/2015/02/15/09/33/news-636978_960_720.jpg"} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href={newUrl} target=" _blank" className="btn btn-sm btn-dark">Load More</a>
+                </div>
             </div>
+           </div>
         )
     }
 }
